add57a8ee50652ad00ad3383662c6876a7e03f44
Yoo Matsuo <yooontheearth@gmail.com>
2015-02-19 10:24:56 +0900

    Initial commit

---
+++ MissingReferenceFinder/MissingReferenceFinder/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Xml;
+
+namespace MissingReferenceFinder
+{
+    public partial class MainWindow : Window
+    {
+        private const string PreferenceFileName = "preferences.xml";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var viewModel = new MainViewModel();
+            DataContext = viewModel;
+            Title += Assembly.GetExecutingAssembly().GetName().Version;
+
+            ReadPreferences(viewModel);
+            Closing += (sender, args) => SavePreferences(viewModel);
+        }
+
+        private void ReadPreferences(MainViewModel mainViewModel)
+        {
+            if (File.Exists(PreferenceFileName))
+            {
+                var doc = new XmlDocument();
+                doc.Load(PreferenceFileName);
+                mainViewModel.DirectoryPath = doc["DirectoryPath"].InnerText;
+            }
+        }
+
+        private void SavePreferences(MainViewModel mainViewModel)
+        {
+            var doc = new XmlDocument();
+            var elem = doc.CreateElement("DirectoryPath");
+            elem.InnerText = mainViewModel.DirectoryPath;
+            doc.AppendChild(elem);
+            doc.Save(PreferenceFileName);
+        }
+    }
+}
